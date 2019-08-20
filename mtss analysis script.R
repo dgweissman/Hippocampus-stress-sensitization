5532b4bc76abbe947cc28a441ce8ff84b4cf8e5c
David Weissman <47182392+dgweissman@users.noreply.github.com>
2019-08-20 11:41:40 -0400

    Update mtss analysis script.R

--- mtss analysis script.R
+++ mtss analysis script.R
@@ -1,52 +1,86 @@
 #Read in data
-mtss<-read.csv("~/OneDrive - Harvard University/Research/Postdoc/MT/MT Hippocampus stress sensitization/Data/mtss.csv")
-#Just Neuroimaging Sample
-mtss2<-mtss[-which(is.na(mtss$Hippocampus)),]
+mtss<-read.csv("mtss.csv")
 
 #Descriptives Table
 #Sex
-table(mtss2$SEX,mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test(mtss2$SEX,mtss2$ANY_ABUSE_CHILD_DV)
+table(mtss$SEX,mtss$ANY_ABUSE_CHILD_DV)
+chisq.test(mtss$SEX,mtss$ANY_ABUSE_CHILD_DV)
 #Race
-table(mtss2$RACE,mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test((mtss2$RACE==2),mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test((mtss2$RACE==3),mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test((mtss2$RACE==4),mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test((mtss2$RACE==5),mtss2$ANY_ABUSE_CHILD_DV)
+table(mtss$RACE,mtss$ANY_ABUSE_CHILD_DV)
+chisq.test((mtss$RACE==2),mtss$ANY_ABUSE_CHILD_DV)
+chisq.test((mtss$RACE==3),mtss$ANY_ABUSE_CHILD_DV)
+chisq.test((mtss$RACE==4),mtss$ANY_ABUSE_CHILD_DV)
+chisq.test((mtss$RACE==5),mtss$ANY_ABUSE_CHILD_DV)
 #Poverty
-table(mtss2$POVERTY,mtss2$ANY_ABUSE_CHILD_DV)
-chisq.test(mtss2$POVERTY,mtss2$ANY_ABUSE_CHILD_DV)
+table(mtss$POVERTY,mtss$ANY_ABUSE_CHILD_DV)
+chisq.test(mtss$POVERTY,mtss$ANY_ABUSE_CHILD_DV)
 #Age
-t.test(mtss2$S3AGE~mtss2$ANY_ABUSE_CHILD_DV)
-sd(mtss2$S3AGE[which(mtss2$ANY_ABUSE_CHILD_DV==1)])
-sd(mtss2$S3AGE[which(mtss2$ANY_ABUSE_CHILD_DV==0)])
+t.test(mtss$S3AGE~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$S3AGE[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$S3AGE[which(mtss$ANY_ABUSE_CHILD_DV==0)])
+#Stressful Life Events
+t.test(mtss$S3AGE~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$S3AGE[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$S3AGE[which(mtss$ANY_ABUSE_CHILD_DV==0)])
 #Hippocampus
-t.test(mtss2$Hippocampus~mtss2$ANY_ABUSE_CHILD_DV)
-sd(mtss2$Hippocampus[which(mtss2$ANY_ABUSE_CHILD_DV==1)])
-sd(mtss2$Hippocampus[which(mtss2$ANY_ABUSE_CHILD_DV==0)])
+mtss$Hippocampus<-mtss$Hippocampus/1000
+t.test(mtss$Hippocampus~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$Hippocampus[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$Hippocampus[which(mtss$ANY_ABUSE_CHILD_DV==0)])
+#Amygdala
+mtss$Amygdala<-mtss$Amygdala/1000
+t.test(mtss$Amygdala~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$Amygdala[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$Amygdala[which(mtss$ANY_ABUSE_CHILD_DV==0)])
 #IC Volume
-t.test(mtss2$ICVol~mtss2$ANY_ABUSE_CHILD_DV)
-sd(mtss2$ICVol[which(mtss2$ANY_ABUSE_CHILD_DV==1)])
-sd(mtss2$ICVol[which(mtss2$ANY_ABUSE_CHILD_DV==0)])
+mtss$ICVol<-mtss$ICVol/100000
+t.test(mtss$ICVol~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$ICVol[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$ICVol[which(mtss$ANY_ABUSE_CHILD_DV==0)])
+
+#Stressful Life Events
+t.test(mtss$TotalStress~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$TotalStress[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$TotalStress[which(mtss$ANY_ABUSE_CHILD_DV==0)])
+
+t.test(mtss$TotalStress_FU~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$TotalStress_FU[which(mtss$ANY_ABUSE_CHILD_DV==1)],na.rm=T)
+sd(mtss$TotalStress_FU[which(mtss$ANY_ABUSE_CHILD_DV==0)],na.rm=T)
+
+#Depression
+t.test(mtss$CDI_TOT~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$CDI_TOT[which(mtss$ANY_ABUSE_CHILD_DV==1)])
+sd(mtss$CDI_TOT[which(mtss$ANY_ABUSE_CHILD_DV==0)])
+
+t.test(mtss$CDI_TOT_FU~mtss$ANY_ABUSE_CHILD_DV)
+sd(mtss$CDI_TOT_FU[which(mtss$ANY_ABUSE_CHILD_DV==1)],na.rm=T)
+sd(mtss$CDI_TOT_FU[which(mtss$ANY_ABUSE_CHILD_DV==0)],na.rm=T)
 
 #Impute missing poverty data
 require(mice)
 set.seed(123)
-mtss3<-mtss2[,c(2,5,6,7,8,12,13,22,23,27,28)]
-w<-is.na(mtss3)
+mtss2<-mtss[,2:14]
+w<-is.na(mtss2)
 w[,8]<-FALSE
 w[,9]<-FALSE
-imp<-mice(mtss3,m=100,where=w)
+w[,13]<-FALSE
+imp<-mice(mtss2,m=100,where=w)
 
 #Analyses
 #Stress Sensitization to depression
 summary(pool(with(imp, lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+scale(TotalStress,scale=F)*ANY_ABUSE_CHILD_DV))))
-summary(pool(with(imp, lm(CDI_TOT_FU~CDI_TOT+SEX+S3AGE+POVERTY+nonwhite+scale(TotalStress_FU,scale=F)*ANY_ABUSE_CHILD_DV))))
-summary(lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+scale(TotalStress,scale=F),data=mtss2[which(mtss2$ANY_ABUSE_CHILD_DV==1),]))
-#Maltreatment and Hippocampal Volume
-summary(pool(with(imp, lm(Hippocampus~SEX+POVERTY+nonwhite+ICVol+ANY_ABUSE_CHILD_DV*scale(S3AGE,scale=F)))))
+summary(pool(with(imp, lm(CDI_TOT_FU~CDI_TOT+SEX+S3AGE+POVERTY+nonwhite+scale(TotalStress_FU,scale=F)*ANY_ABUSE_CHILD_DV+DAYS_S1))))
+#Violence and Hippocampal Volume
+summary(pool(with(imp, lm(Hippocampus~POVERTY+nonwhite+ICVol+ANY_ABUSE_CHILD_DV))))
+#Violence and Amygdala Volume
+summary(pool(with(imp, lm(Amygdala~POVERTY+nonwhite+ICVol+ANY_ABUSE_CHILD_DV))))
 #Hipocampal volume and depression
-summary(pool(with(imp, lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+Hippocampus+ICVol))))
+summary(pool(with(imp, lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+ICVol+Hippocampus))))
+summary(pool(with(imp, lm(CDI_TOT_FU~CDI_TOT+SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+ICVol+DAYS_S1+Hippocampus))))
+#Amygdala volume and depression
+summary(pool(with(imp, lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+ICVol+Amygdala))))
+summary(pool(with(imp, lm(CDI_TOT_FU~CDI_TOT+SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+ICVol+DAYS_S1+Amygdala))))
 #Hippocampal volume x Stressful life events 
-summary(pool(with(imp, lm(CDI_TOT~SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+scale(TotalStress,scale=F)*scale(Hippocampus,scale=F)+ICVol))))
-summary(pool(with(imp, lm(CDI_TOT_FU~CDI_TOT+SEX+S3AGE+POVERTY+nonwhite+ANY_ABUSE_CHILD_DV+scale(TotalStress_FU,scale=F)*scale(Hippocampus,scale=F)+ICVol))))
+summary(pool(with(imp, lm(CDI_TOT_FU~SEX+S3AGE+DAYS_S1+nonwhite+POVERTY+CDI_TOT+ANY_ABUSE_CHILD_DV+ICVol+scale(TotalStress_FU,scale=F)*scale(Hippocampus,scale=F)))))
+#Amygdala volume x Stressful life events
+summary(pool(with(imp, lm(CDI_TOT_FU~SEX+S3AGE+DAYS_S1+nonwhite+POVERTY+CDI_TOT+ANY_ABUSE_CHILD_DV+ICVol+scale(TotalStress_FU,scale=F)*scale(Amygdala,scale=F)))))
